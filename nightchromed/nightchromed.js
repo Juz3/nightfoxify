f20a049508741c57d86e632f503ca61f6ee64a94
Juz3 <jussi.koivumaki@outlook.com>
2021-03-01 20:06:54 +0200

    added more html elements to styles

--- nightchromed/nightchromed.js
+++ nightchromed/nightchromed.js
@@ -27,7 +27,7 @@ function setBgMode(toggle) {
       background-color: rgba(22, 23, 26, 0.9) !important;
       color: #FFF !important;
     }
-    article, button, div, h1, h2, h3, header, figure, input, li, nav, pre, section, tbody, th, tr, td, table, ul {
+    article, button, div, figure, form, h1, h2, h3, h4, h5, header, input, li, nav, pre, section, tbody, th, tr, td, table, ul {
       background-color: rgba(31, 32, 34, 0.9) !important;
       color: #FFF !important;
     }
